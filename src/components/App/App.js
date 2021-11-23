3e4ce570339e5bbc57eb6049450a1d04b3323e8e
Liubas <olha.liubas@sigmaukraine.com>
2021-11-23 17:47:52 +0200

    feature/fetching data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,29 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import PropTypes from "prop-types";
 
 import Row from "components/Row/";
 import Footer from "components/Footer";
 import Header from "components/Header";
-import countries from "constants/countries";
 
 import styles from "./App.module.scss";
 
 const App = () => {
-  return (
+  const [countries, setCountries] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await axios.get("http://localhost:4000/countries");
+        setCountries(response.data);
+      } catch (e) {
+        alert(`Sorry, something go wrong. Try later!`);
+      }
+    }
+    getData();
+  }, []);
+
+  return countries ? (
     <div className={styles.table}>
       <Header />
 
@@ -18,6 +33,8 @@ const App = () => {
 
       <Footer />
     </div>
+  ) : (
+    <p>Loading ...</p>
   );
 };
 
