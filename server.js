51279f1ac3ae149b218bc92dce08717fa528a6b2
Liubas <olha.liubas@sigmaukraine.com>
2021-12-28 21:15:39 +0200

    bugFix/serverChanges

--- server.js
+++ server.js
@@ -1,24 +1,43 @@
 const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
-const middlewares = jsonServer.defaults({
-  static: "./build",
-});
-const PORT = process.env.PORT || 4000;
-server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-  })
-);
-server.use(router);
-server.get("/*", (req, res) => {
-  res.sendFile(__dirname + "/build/index.html");
+const app = jsonServer.create();
+const path = require("path");
+const express = require("express");
+const middlewares = jsonServer.defaults();
+const router = jsonServer.router("db.json");
+const port = process.env.PORT || 3001;
+
+app.use("/db", middlewares, router);
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-server.listen(PORT, () => {
+app.listen(port, () => {
   console.log("Server is running");
 });
+
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router("./db.json");
+// const middlewares = jsonServer.defaults({
+//   static: "./build",
+// });
+// const PORT = process.env.PORT || 4000;
+// server.use(middlewares);
+// server.use(
+//   jsonServer.rewriter({
+//     "/api/*": "/$1",
+//   })
+// );
+// server.get("/*", (req, res) => {
+//   res.sendFile(__dirname + "/build/index.html");
+// });
+// server.use(router);
+
+// server.listen(PORT, () => {
+//   console.log("Server is running");
+// });
 
 // const jsonServer = require("json-server");
 // const server = jsonServer.create();
