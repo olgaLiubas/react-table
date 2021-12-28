4b52bf4850c107559b2799d9a5745e223ab5f500
Liubas <olha.liubas@sigmaukraine.com>
2021-12-28 20:04:39 +0200

    bugFix/serverChanges

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ server.use(
     "/api/*": "/$1",
   })
 );
-server.use(router);
+server.use("/api", router);
+// server.use(router);
 server.listen(PORT, () => {
   console.log("Server is running");
 });
