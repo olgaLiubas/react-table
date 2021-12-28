35a4397b1dce7979532ad96f0221195fc5434356
Liubas <olha.liubas@sigmaukraine.com>
2021-12-28 20:20:29 +0200

    bugFix/serverChanges

--- server.js
+++ server.js
@@ -8,11 +8,33 @@ const PORT = process.env.PORT || 4000;
 server.use(middlewares);
 server.use(
   jsonServer.rewriter({
-    "/api/*": "/$1",
+    "/api/*": "/",
   })
 );
-server.use("/api", router);
-// server.use(router);
+server.use(router);
+// server.use("/api", router);
+// server.get("*", function (req, res) {
+//   //   res.redirect("/");
+//   res.sendfile(__dirname + "/");
+// });
 server.listen(PORT, () => {
   console.log("Server is running");
 });
+
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router("./db.json");
+// const middlewares = jsonServer.defaults({
+//   static: "./build",
+// });
+// const PORT = process.env.PORT || 4000;
+// server.use(middlewares);
+// server.use(
+//   jsonServer.rewriter({
+//     "/api/*": "/$1",
+//   })
+// );
+// server.use(router);
+// server.listen(PORT, () => {
+//   console.log("Server is running");
+// });
